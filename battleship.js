7f836054a8d124802542a0adaaf729fede781cc1
Nikita <108724726+NikTi-it@users.noreply.github.com>
2022-08-20 13:43:40 +0300

    Add files via upload

--- battleship.js
+++ battleship.js
@@ -1,5 +1,5 @@
 //Логика для кораблей
-var randomLoc = Math.floor(Math.random() * 5);
+var randomLoc = Math.floor((Math.random() * 5) + 1);
 var location1 = randomLoc;
 var location2 = location1 + 1;
 var location3 = location2 + 1;
@@ -7,16 +7,22 @@ var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
-var emptyinput = /^[0-9]/; //Переменная для проверки пустого или некорректного ввода
+var emptyinput =/(?<![\.\d])\d+(?![\.\d])/; //Переменная для проверки пустого или некорректного ввода
 var usedone = new Array(); //Массив для проверки повторной атаки в ячейку
 while (isSunk == false) {
-    guess = prompt("Привет! Это мини морской бой, в семи ячейках ООООООО (от нуля до шести) спрятан вражеский трёхпалубный корабль OOO, нужно потопить его за меньшее количество попыток. Готовьсь, цельсь, пли! (Введи числов 0-6):");
-    if (emptyinput.test(guess) == false) { //Проверка пустого или некорректного ввода
+    guess = prompt("Привет! Это мини морской бой, в семи ячейках ООООООО (от одного до семи) спрятан вражеский трёхпалубный корабль OOO, нужно потопить его за меньшее количество попыток. Готовьсь, цельсь, пли! (Введи число 1-7):");
+    if (guess == null) {
+        (alert("Игра отменена, активировано самоуничтожение корабля, всего доброго!"));
+        window.close();
+        break; // Действия в случае нажатия отмены
+    } else if (emptyinput.test(guess) == false) { //Проверка пустого или некорректного ввода
         alert("Пустой или некорректный ввод! Введи корректный номер ячейки!");
-    } else if  (guess < 0 || guess > 6) {
+    } else if  (guess < 1 || guess > 7) {
         alert("Введи корректный номер ячейки!");
     } else if (usedone.includes(guess)) {
         alert("Сюды ты уже стрелял!");
+    } else if (guess == null) {
+        alert("Игра отменена, для повторного запуска обнови страницу");
     } else {
         guesses = guesses + 1;
         if (guess == location1 || guess == location2 || guess == location3) {
@@ -33,5 +39,6 @@ while (isSunk == false) {
     }
 }
 
-var stats = "Использовано попыток для потопления корабля -  " + guesses +", " + "точность составляет " + Math.round(3/guesses*100)+"%";
-alert(stats);
+if (isSunk == true) {
+    var stats = "Использовано попыток для потопления корабля -  " + guesses +", " + "точность составляет " + Math.round(3/guesses*100)+"%! \n\nЧтобы сыграть снова обнови страницу.";
+    alert(stats);}
